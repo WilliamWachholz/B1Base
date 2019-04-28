38b1db79dcb58f77d8170e2d13f9f3b81b0b2fa5
william.wachholz <william.wachholz@eafdeada-f308-9343-9a48-94a505fd8ed7>
2019-04-28 10:37:56 +0000

    Criação de Serviço Windows
    git-svn-id: https://desktop-k8125me/svn/B1/B1Base@503 eafdeada-f308-9343-9a48-94a505fd8ed7

---
+++ B1Base/Controller/ServiceController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using Microsoft.Win32;
+using System.Configuration;
+
+namespace B1Base.Controller
+{
+    public abstract class ServiceController : ServiceBase
+    {
+        static System.Timers.Timer timer;
+
+        public abstract string ServiceName { get; }
+        public abstract string ServiceTitle { get; }
+        public abstract string ServiceDescription { get; }
+
+        protected abstract void Execute();
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                EventLog.WriteEntry(ServiceName, "Starting.", System.Diagnostics.EventLogEntryType.Information);
+
+                B1Base.Controller.ConnectionController.Instance.Initialize(ConfigurationSettings.AppSettings.Get("AddOnId"),
+                    ConfigurationSettings.AppSettings.Get("Server"),
+                    ConfigurationSettings.AppSettings.Get("CompanyDB"),
+                    ConfigurationSettings.AppSettings.Get("UserName"),
+                    ConfigurationSettings.AppSettings.Get("Password"),
+                    ConfigurationSettings.AppSettings.Get("LicenseServer"),
+                    ConfigurationSettings.AppSettings.Get("DBUserName"),
+                    ConfigurationSettings.AppSettings.Get("DBPassword"),
+                    ConfigurationSettings.AppSettings.Get("DBServerType"));
+
+
+                timer = new System.Timers.Timer(Convert.ToInt32(ConfigurationSettings.AppSettings.Get("TimeInMiliseconds")));
+                timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Tick);
+                timer.Enabled = true;
+                timer.Start();
+
+                EventLog.WriteEntry(ServiceName, "Started.", System.Diagnostics.EventLogEntryType.SuccessAudit);
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry(ServiceName, "Starting error: " + exception.Message, System.Diagnostics.EventLogEntryType.FailureAudit);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            EventLog.WriteEntry(ServiceName, "Stoping.", System.Diagnostics.EventLogEntryType.Information);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                EventLog.WriteEntry(ServiceName, "Executing.", System.Diagnostics.EventLogEntryType.Information);
+
+                Execute();
+
+                EventLog.WriteEntry(ServiceName, "Executed.", System.Diagnostics.EventLogEntryType.SuccessAudit);
+            }
+            catch (Exception exception)
+            {
+                EventLog.WriteEntry(ServiceName, "Execution error: " + exception.Message, System.Diagnostics.EventLogEntryType.FailureAudit);
+            }
+        }
+
+
+
+
+
+
+
+        
+
+    }
+}
---
+++ B1Base/Service.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using Microsoft.Win32;
+
+namespace B1Base
+{
+    public class Service
+    {
+        static readonly Service m_Instance = new Service();
+
+        public static Service Instance
+        {
+            get { return m_Instance; }
+        }
+
+        Controller.ServiceController m_ServiceController = null;
+
+        public Controller.ConnectionController ConnectionController { get { return Controller.ConnectionController.Instance; } }
+
+        public void Initialize(Controller.ServiceController serviceController)
+        {
+            m_ServiceController = serviceController;
+
+            try
+            {
+                if (ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == m_ServiceController.ServiceName) == null)
+                {
+                    Install();
+                }
+                else
+                {
+                    bool uninstall = Environment.GetCommandLineArgs().Count() > 1 && Environment.GetCommandLineArgs().GetValue(1).ToString() == "uninstall";
+
+                    if (uninstall)
+                    {
+                        Uninstall();
+                    }
+                    else
+                    {
+                        EventLog.WriteEntry(m_ServiceController.ServiceName, "Iniciado", EventLogEntryType.Information);
+                        ServiceBase[] ServicesToRun;
+                        ServicesToRun = new ServiceBase[] { (ServiceBase)m_ServiceController };
+
+                        ServiceBase.Run(ServicesToRun);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                EventLog.WriteEntry(m_ServiceController.ServiceName, "General Failure: " + ex.Message, System.Diagnostics.EventLogEntryType.Information);
+            }
+        }
+
+        void Install()
+        {
+            IntegratedServiceInstaller integratedServiceInstaller = new IntegratedServiceInstaller();
+            integratedServiceInstaller.Install(m_ServiceController.ServiceName, m_ServiceController.ServiceTitle, m_ServiceController.ServiceDescription,
+                System.ServiceProcess.ServiceAccount.LocalSystem,
+                System.ServiceProcess.ServiceStartMode.Automatic);
+        }
+
+        void Uninstall()
+        {
+            IntegratedServiceInstaller integratedServiceInstaller = new IntegratedServiceInstaller();
+            integratedServiceInstaller.Uninstall(m_ServiceController.ServiceName);
+        }
+
+        class IntegratedServiceInstaller
+        {
+            public void Install(String ServiceName, String DisplayName, String Description,
+                System.ServiceProcess.ServiceAccount Account,
+                System.ServiceProcess.ServiceStartMode StartMode)
+            {
+                System.ServiceProcess.ServiceProcessInstaller ProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+                ProcessInstaller.Account = Account;
+
+                System.ServiceProcess.ServiceInstaller SINST = new System.ServiceProcess.ServiceInstaller();
+
+                System.Configuration.Install.InstallContext Context = new System.Configuration.Install.InstallContext();
+                string processPath = Process.GetCurrentProcess().MainModule.FileName;
+                if (processPath != null && processPath.Length > 0)
+                {
+                    System.IO.FileInfo fi = new System.IO.FileInfo(processPath);
+
+                    String path = String.Format("/assemblypath={0}", fi.FullName);
+                    String[] cmdline = { path };
+                    Context = new System.Configuration.Install.InstallContext("", cmdline);
+                }
+
+                SINST.Context = Context;
+                SINST.DisplayName = DisplayName;
+                SINST.Description = Description;
+                SINST.ServiceName = ServiceName;
+                SINST.StartType = StartMode;
+                SINST.Parent = ProcessInstaller;
+
+                System.Collections.Specialized.ListDictionary state = new System.Collections.Specialized.ListDictionary();
+                SINST.Install(state);
+
+                using (RegistryKey oKey = Registry.LocalMachine.OpenSubKey(String.Format(@"SYSTEM\CurrentControlSet\Services\{0}", SINST.ServiceName), true))
+                {
+                    try
+                    {
+                        Object sValue = oKey.GetValue("ImagePath");
+                        oKey.SetValue("ImagePath", sValue);
+                    }
+                    catch (Exception Ex)
+                    {
+
+                    }
+                }
+
+            }
+
+            public void Uninstall(String ServiceName)
+            {
+                System.ServiceProcess.ServiceInstaller SINST = new System.ServiceProcess.ServiceInstaller();
+
+                System.Configuration.Install.InstallContext Context = new System.Configuration.Install.InstallContext("c:\\install.log", null);
+                SINST.Context = Context;
+                SINST.ServiceName = ServiceName;
+                SINST.Uninstall(null);
+            }
+        }
+    }
+}
