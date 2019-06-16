0ca7f631ee7daae05951d2e97aa1b1ac911f1dd4
william.wachholz <william.wachholz@eafdeada-f308-9343-9a48-94a505fd8ed7>
2019-06-16 20:33:35 +0000

    release
    git-svn-id: https://desktop-k8125me/svn/B1/B1Base@680 eafdeada-f308-9343-9a48-94a505fd8ed7

---
+++ B1Base/bin/x86/Release/SQL/SQLSERVER/GetCustomizedTemplate.sql
@@ -0,0 +1,8 @@
+﻿select  "Left", 
+		"Top", 
+		"Editable",
+		"Visible"
+from UIC2
+inner join UICU on (UICU."TPLId" = UIC2."TPLId" and UICU."UserID" = {0})
+where UIC2."FormId" = '{1}'
+and UIC2."ItemId" = '{2}'
