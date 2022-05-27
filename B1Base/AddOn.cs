875b7110a53e7e0a0116e084ab2096e165786e9d
WilliamWachholz <william.wachholz@gmail.com>
2022-05-27 10:29:28 -0300

    code to kill processs if B1 crashes

--- B1Base/AddOn.cs
+++ B1Base/AddOn.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SAPbouiCOM;
+using System.Timers;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace B1Base
 {
@@ -29,6 +33,13 @@ namespace B1Base
         {
             this.MainController = mainController;
             this.MainController.Initialize();
+
+            orgPID = getProcessID();
+
+            System.Timers.Timer t = new System.Timers.Timer(60000);
+            t.AutoReset = true;
+            t.Elapsed += new System.Timers.ElapsedEventHandler(t_Elapsed);
+            t.Start();
         }
 
         public string CurrentDirectory
@@ -57,6 +68,58 @@ namespace B1Base
                 }
             }
             System.Diagnostics.Process.Start(System.IO.Path.Combine(B1Base.AddOn.Instance.CurrentDirectory, file));
-        }        
+        }
+
+
+        int orgPID { get; set; }
+
+        private void t_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var res = getProcessID();
+
+            if (res != orgPID)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+
+        private int getProcessID()
+        {
+            var gui = new SboGuiApi();
+            try
+            {
+                gui.Connect("0030002C0030002C00530041005000420044005F00440061007400650076002C0050004C006F006D0056004900490056");
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+
+            var appId = gui.GetApplication().AppId;
+
+            var processes = Process.GetProcessesByName(@"SAP Business One").Where(x => x.SessionId == Process.GetCurrentProcess().SessionId);
+
+
+            foreach (var process in processes)
+            {
+                try
+                {
+                    var processAppId = gui.GetAppIdFromProcessId(process.Id);
+                    if (appId == processAppId)
+                    {
+                        return process.Id;
+                    }
+                }
+                catch (COMException)
+                {
+                    // GetAppIdFromProcessId will throw when the current process is not the one we are connected to
+                    continue;
+                }
+            }
+
+            return -1;
+        }
     }
 }
